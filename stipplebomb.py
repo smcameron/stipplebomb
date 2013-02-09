7210ab0daee50b1e76fa68e6018e3a8a436d77a6
Stephen M. Cameron <stephenmcameron@gmail.com>
2013-02-09 10:09:07 -0600

    Draw dots with radii proportional to corresponding image coords

--- stipplebomb.py
+++ stipplebomb.py
@@ -25,6 +25,8 @@ import pygame
 import time
 import os, sys
 import Image
+import random
+import math
 
 im = Image.open("image.jpg")
 
@@ -33,15 +35,55 @@ y = 4
 
 pix = im.load()
 print pix[x,y]
-image_width = im.size[0];
-image_height = im.size[1];
+image_width = 0.0 + im.size[0];
+image_height = 0.0 + im.size[1];
+screen_width = 1300
+screen_height = 700
+scaling_factor = (0.0 + screen_height) / (0.0 + image_height);
+
+# origin on screen
+osx = (screen_width / 2.0) - (image_width / 2.0) * scaling_factor;
+osy = 0.0;
+
 print "w = ", image_width, "h = ", image_height
 
-screen = pygame.display.set_mode((640,480))
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.update()
 
-pygame.draw.circle(screen, (255,255,255), (100, 100), 50, 0);
+# screen x to image x
+def sxtoix(sx):
+  return (sx - osx) / scaling_factor;
+
+def sytoiy(sy):
+  return (sy - osy) / scaling_factor;
+
+def sampleimg(sx, sy):
+   ix = sxtoix(sx)
+   iy = sytoiy(sy)
+   if ix < 0:
+      return 0
+   if iy < 0:
+      return 0
+   if ix >= image_width:
+      return 0
+   if iy >= image_height:
+      return 0
+   return pix[ix, iy]
+
+# image intensity to circle radius
+def itor(i):
+   return ((0.0 + i) / 255.0) * 5.0;
+    
+def circle(x, y, r):
+	pygame.draw.circle(screen, (255,255,255), (x, y), r, 0);
+
+for i in range(0, 10000):
+  tx = random.randint(0, screen_width);
+  ty = random.randint(0, screen_height);
+  r = itor(sampleimg(tx, ty));
+  circle(tx, ty, int(r));
+
 pygame.display.update()
 
-time.sleep(10)
+time.sleep(2)
 
